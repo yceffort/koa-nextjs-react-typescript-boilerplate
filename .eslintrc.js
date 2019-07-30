f65913d0ea84ff77fd88ec808dfacb984432822f
yceffort <yceffort@gmail.com>
2019-07-30 16:51:24 +0900

    ✨ add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    '@typescript-eslint/indent': ['warning', 2],
+    '@typescript-eslint/semi': ['never'],
+    '@typescript-eslint/quotes': ['single'],
+    '@typescript-eslint/member-delimiter-style': 'none',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/camelcase': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 ## dependencies
 
-- "koa": "^2.7.0"
-- "koa-body": "^4.1.0"
-- "koa-morgan": "^1.0.1"
-- "koa-mount": "^4.0.0"
-- "koa-proxies": "^0.8.1"
-- "koa-router": "^7.4.0"
-- "next": "latest"
-- "react": "^16.8.4"
-- "react-dom": "^16.8.4"
-- "styled-components": "^4.3.2"
+- koa@^2.7.0
+- koa-body@^4.1.0
+- koa-morgan@^1.0.1
+- koa-mount@^4.0.0
+- koa-proxies@^0.8.1
+- koa-router@^7.4.0
+- next@latest
+- react@^16.8.4
+- react-dom@^16.8.4
+- styled-components@^4.3.2
 
