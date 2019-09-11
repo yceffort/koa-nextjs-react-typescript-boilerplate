1562b5e646e5e1d7d2aec1186f71f5567ff3eb42
yceffort <yceffort@gmail.com>
2019-09-11 10:18:47 +0900

    👕 lint

--- next.config.js
+++ next.config.js
@@ -1,30 +1,30 @@
-const TerserPlugin = require("terser-webpack-plugin");
+import TerserPlugin from 'terser-webpack-plugin'
 
 module.exports = {
   generateEtags: true,
-  webpack: config => {
+  webpack: (config) => {
     config.optimization.minimizer = [
       new TerserPlugin({
         terserOptions: {
-          safari10: true
-        }
-      })
-    ];
+          safari10: true,
+        },
+      }),
+    ]
 
-    const originalEntry = config.entry;
+    const originalEntry = config.entry
     config.entry = async () => {
-      const entries = await originalEntry();
+      const entries = await originalEntry()
 
       if (
-        entries["main.js"] &&
-        !entries["main.js"].includes("./src/utils/polyfills.js")
+        entries['main.js'] &&
+        !entries['main.js'].includes('./src/utils/polyfills.js')
       ) {
-        entries["main.js"].unshift("./src/utils/polyfills.js");
+        entries['main.js'].unshift('./src/utils/polyfills.js')
       }
 
-      return entries;
-    };
+      return entries
+    }
 
-    return config;
-  }
-};
+    return config
+  },
+}
--- typings/koa-proxies/index.d.ts
+++ typings/koa-proxies/index.d.ts
@@ -1,6 +1,6 @@
-declare module "koa-proxies" {
-  import { Middleware } from "koa";
+declare module 'koa-proxies' {
+  import { Middleware } from 'koa'
   namespace koaProxies {}
-  function koaProxies(name: string, options?: any): Middleware;
-  export = koaProxies;
+  function koaProxies(name: string, options?: any): Middleware
+  export = koaProxies
 }
