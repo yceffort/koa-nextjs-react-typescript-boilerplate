89d779790990b322a52457cf79143fa125e89b77
yceffort <yceffort@gmail.com>
2020-02-07 21:26:08 +0900

    🗑 remove custom typing

--- typings/koa-proxies/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'koa-proxies' {
-  import { Middleware } from 'koa'
-  namespace koaProxies {}
-  function koaProxies(name: string, options?: any): Middleware
-  export = koaProxies
-}
