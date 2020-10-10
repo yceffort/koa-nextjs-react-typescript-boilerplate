ad564f7f54bff3af433e22e659abc4d75ac09368
yceffort <yceffort@gmail.com>
2020-10-10 22:14:53 +0900

    Apply eslint-config-yceffort

--- src/utils/polyfills.js
+++ src/utils/polyfills.js
@@ -1,38 +1,38 @@
-import "core-js/features/set";
-import "core-js/features/map";
-import "core-js/features/symbol";
-import "core-js/features/array";
-import "core-js/features/object";
+import 'core-js/features/set'
+import 'core-js/features/map'
+import 'core-js/features/symbol'
+import 'core-js/features/array'
+import 'core-js/features/object'
 
 function addCustomEventPolyfill() {
-  if (typeof window.CustomEvent === "function") {
+  if (typeof window.CustomEvent === 'function') {
     // If not IE
-    return false;
+    return false
   }
 
   function CustomEvent(
     event,
-    params = { bubbles: false, cancelable: false, detail: undefined }
+    params = { bubbles: false, cancelable: false, detail: undefined },
   ) {
-    const { bubbles, cancelable, detail } = params;
-    const customEvent = document.createEvent("CustomEvent");
+    const { bubbles, cancelable, detail } = params
+    const customEvent = document.createEvent('CustomEvent')
 
-    customEvent.initCustomEvent(customEvent, bubbles, cancelable, detail);
+    customEvent.initCustomEvent(customEvent, bubbles, cancelable, detail)
 
-    return customEvent;
+    return customEvent
   }
 
-  CustomEvent.prototype = window.Event.prototype;
+  CustomEvent.prototype = window.Event.prototype
 
-  window.CustomEvent = CustomEvent;
+  window.CustomEvent = CustomEvent
 }
 
 function addArrayFlatPolyfill() {
-  const ArrayPrototype = Array.prototype;
+  const ArrayPrototype = Array.prototype
 
-  if ("flatten" in ArrayPrototype && !("flat" in ArrayPrototype)) {
-    ArrayPrototype.flat = ArrayPrototype.flatten;
+  if ('flatten' in ArrayPrototype && !('flat' in ArrayPrototype)) {
+    ArrayPrototype.flat = ArrayPrototype.flatten
   }
 }
-addCustomEventPolyfill();
-addArrayFlatPolyfill();
+addCustomEventPolyfill()
+addArrayFlatPolyfill()
