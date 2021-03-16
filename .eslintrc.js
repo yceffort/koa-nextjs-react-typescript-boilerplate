5597cfaa16649307daa58c85524da78bb5b87eb4
yceffort <yceffort@gmail.com>
2021-03-16 16:13:14 +0900

    👔 run prettier & lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,9 @@
 const customRule = require('./naming-convention')
 
 module.exports = {
-  "extends": [
-    "eslint-config-yceffort/typescript",
-  ],
-  "rules": {
-    '@typescript-eslint/naming-convention': customRule.extendedRules
-    'react/react-in-jsx-scope': ['off']
-  }
+  extends: ['eslint-config-yceffort/typescript'],
+  rules: {
+    '@typescript-eslint/naming-convention': customRule.extendedRules,
+    'react/react-in-jsx-scope': ['off'],
+  },
 }
