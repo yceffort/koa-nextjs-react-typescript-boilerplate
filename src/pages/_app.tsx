12e90e1b646e25b593c4728242eebbb50447c33c
yceffort <yceffort@gmail.com>
2021-03-16 15:53:20 +0900

    ⚙️move viewport tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,15 @@ import { AppProps } from 'next/app'
 import 'normalize.css'
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, minimum-scale=1, viewport-fit=cover"
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
